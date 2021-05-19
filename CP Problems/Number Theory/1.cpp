44f90ca60d1792f287c433b600c7d8f4420a3957
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-05-19 10:58:00 +0530

    Create 1.cpp

---
+++ CP Problems/Number Theory/1.cpp
@@ -0,0 +1,25 @@
+//Question 
+https://www.codechef.com/MAY21C/problems/ISS
+
+//Editorial
+https://discuss.codechef.com/t/iss-editorial/89418
+
+//Solve Similer Problem
+https://www.spoj.com/problems/LCMSUM/
+https://www.spoj.com/problems/GCDEX/
+
+
+//Concept
+//Number Theory Lactures of UTKARSH SHARMA at Unacademy
+
+
+//Dificulty
+HARD
+
+
+
+
+
+
+
+.
