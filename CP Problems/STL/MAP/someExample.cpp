732658187f522a0cf1270634ec75d9104c5af150
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-07-27 08:52:12 +0530

    Update someExample.cpp

--- CP Problems/STL/MAP/someExample.cpp
+++ CP Problems/STL/MAP/someExample.cpp
@@ -22,3 +22,51 @@ int main(){
     }
     cout<<maxi<<endl;
 }
+
+
+/* Problem:
+
+   Given N strings and Q queries, 
+   In each query you are given a string
+   print frequency of that string.
+
+   N <= 10^6
+   |S| <= 100
+   Q <= 10^6 
+
+   Input:
+   8
+   abc
+   def
+   op
+   abc
+   op
+   def
+   abc
+   p
+
+   output:
+   abc 3
+   def 2
+   op 2
+   p 1
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   map<string,int> mpp;
+   int n;
+   cin>>n;
+   for(int i=0;i<n;i++){
+      string s;
+      cin>>s;
+      mpp[s]++;
+   }
+   for(auto it:mpp){
+      cout<< it.first<<" "<<it.second<<endl;
+   }
+}
