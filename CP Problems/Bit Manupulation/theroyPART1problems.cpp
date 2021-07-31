1077c05952cf43ed5c4a4dc1e9d8ae6d3945a8c8
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-07-31 09:53:08 +0530

    Create theroyPART1problems.cpp

---
+++ CP Problems/Bit Manupulation/theroyPART1problems.cpp
@@ -0,0 +1,74 @@
+/*
+
+Do this problems-> one by one as striver's codeverse course.
+...................................................................
+
+
+
+
+1.You are given an array {2,2,3,4,1,3,5,5,4},
+   every element occars twice, only one occars one time,
+   Find the once appeared element.
+
+   output: 1 
+
+
+
+
+2.SWAP two number using Bit-manupulation 
+   Input: a=5 b=7
+   Output: a=7 b=5
+
+
+3.Given an integer N, Print XOR of all elements (1 to N).
+   Input: 5
+   Output:1
+   Explanation: 1^2^3^4^5 = 1
+
+
+
+4.Given a range (L to R), Print xor of all elements. 
+   Input: 2 4
+   output: 5
+
+
+5.Check a given number even or odd using Bit-Magic
+
+
+
+6.You are given a integer number.
+   Check i-th bit set or not , set means 1.
+
+   Input: 5
+          2
+   Output:1 (Yes)
+   Input: 5
+          1
+   Output: 0 (No)
+
+
+
+
+
+
+7.Change the i-th bit, of a given number N
+
+
+8.Clear the i-th bit. Given N and i.
+   i can be 0 or 1 whatever, make the bit 0 (clear)
+
+
+9.Remove the last set bit
+
+
+10.How to check a number is power of 2
+
+
+
+12.Count the no of set bits.
+
+
+
+
+
+*/
