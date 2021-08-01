5cd7fe1017dce881a95bc4a4f011151d8f5ab631
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-08-01 10:16:44 +0530

    Update 1.cpp

--- CP Problems/Basic Math/Adhoc/1.cpp
+++ CP Problems/Basic Math/Adhoc/1.cpp
@@ -1,4 +1,11 @@
-https://www.codechef.com/START7C/problems/CEILSUM
+/*
+
+1.  https://www.codechef.com/START7C/problems/CEILSUM
+
+2.  https://www.codechef.com/LTIME97C/problems/REALBIN
+
+ Note: This AND approach give me TLE. https://www.codechef.com/viewsolution/49367593
 
 
 
+*/
