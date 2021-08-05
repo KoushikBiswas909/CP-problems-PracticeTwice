007695477ed8c046ac4b515d4485490468c795c4
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-08-05 12:14:28 +0530

    Update 1.cpp

--- CP Problems/Basic Math/Adhoc/1.cpp
+++ CP Problems/Basic Math/Adhoc/1.cpp
@@ -5,6 +5,10 @@
 2.  https://www.codechef.com/LTIME97C/problems/REALBIN
 
  Note: This AND approach give me TLE. https://www.codechef.com/viewsolution/49367593
+ 
+3.* https://codeforces.com/contest/365/problem/A
+
+          https://codeforces.com/contest/365/submission/124925948
 
 
 
