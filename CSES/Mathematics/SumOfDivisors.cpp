472cbc898ea5b270b3380858f3c90bcedb12e531
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-09-08 21:29:04 +0530

    Update SumOfDivisors.cpp

--- CSES/Mathematics/SumOfDivisors.cpp
+++ CSES/Mathematics/SumOfDivisors.cpp
@@ -32,3 +32,58 @@ int main(){
     }
     cout<<ans<<endl;
 }
+
+
+*************************************************************************************************************************************************************
+// Optimal approach
+
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+const int mod=1e9+7;
+
+// This is also right
+/*ll modularbinaryexponentiation(int base, int exponent){
+  if(exponent==0)
+    return 1;
+  ll result=modularbinaryexponentiation(base,exponent/2);
+  if(exponent%2==1)
+    return (((result*result)%mod)*base)%mod;
+  else
+    return (result*result)%mod;
+}*/
+
+
+// This is also right
+ll modularbinaryexponentiation(int a, int b){
+  if(b==0)
+    return 1%mod;
+  else if(b%2==0){
+    ll x=modularbinaryexponentiation(a,b/2);
+    return (x*x)%mod;
+  }
+  else{
+    ll y=(modularbinaryexponentiation(a,b-1))%mod;
+    return (a*y)%mod;
+  }
+}
+
+int main(){
+  ll n;
+  cin>>n;
+
+  ll answer=0;
+  for(ll i=1,j;i<=n;i=j){
+    ll q=n/i;
+    j=(n/q)+1;
+
+    ll sumuptojminus1=((j%mod*((j-1)%mod))%mod * modularbinaryexponentiation(2,mod-2))%mod;
+    ll sumuptoIminus1=((i%mod*((i-1)%mod))%mod * modularbinaryexponentiation(2,mod-2))%mod;
+    ll rangewithQuotientQ= (sumuptojminus1- sumuptoIminus1+mod)%mod; //When we performming any difference with mod, always use extra +mod, because if diff goes in '-'ve, +mod always keep it in range [0,n).
+    answer= (answer+(q%mod)*rangewithQuotientQ)%mod;
+  }
+  cout<<answer<<endl;
+}
+
+
