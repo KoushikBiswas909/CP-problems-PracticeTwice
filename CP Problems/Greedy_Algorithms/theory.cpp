01b6d2747f542a1cb6ee4caf23ad4683d6f8166d
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-09-18 19:44:05 +0530

    Create theory.cpp

---
+++ CP Problems/Greedy_Algorithms/theory.cpp
@@ -0,0 +1,45 @@
+/*
+*************************  Introduction  ********************************
+
+•Think quietly One problem has Multiple solutions, but there are always a constraints,
+So those problems comes under constraints call -> *Feasible Solutions*.
+•But Feasible solutions can be multiple number.
+•Now from the feasible solutions if you have to choose max or min optimized solutions,
+then the problem is called *Optimized Problem*
+•Optimized Problems can be solved by 3 methods:
+   1. Greedy Method
+   2. Dynamic Programming
+   3. Branch and Bound Method
+
+
+**************************   Greedy Algorithms  ***************************
+
+Greedy algorithms work in stages. In each stage, a decision is made that is good at that point,
+without bothering about the future. 
+
+**************************   Greedy Applications  *************************
+
+• Sorting: Selection sort, Topological sort
+• Priority Queues: Heap sort
+• Huffman coding compression algorithm
+• Prim’s and Kruskal’s algorithms
+• Shortest path in Weighted Graph [Dijkstra’s]
+• Coin change problem
+• Fractional Knapsack problem
+• Disjoint sets-UNION by size and UNION by height (or rank)
+• Job scheduling algorithm
+• Greedy techniques can be used as an approximation algorithm for complex problems
+
+************************  Huffman Coding Algorithm  **********************
+
+I understand the thing write algo in book this pic in repo.
+Didnot code it caz i don't know some priority queue
+Theory from- Abdul Bari
+Algo from- Git repo & Narasimha
+
+
+
+https://leetcode.com/discuss/interview-question/1065228/Job-sequencing-Problem
+https://www.geeksforgeeks.org/job-sequencing-problem/
+
+*/
