fa298e7e1592069c1f761486f9c1c432314d2887
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-09-18 19:45:26 +0530

    Create theory.cpp

---
+++ Aptitude/Topics/HCF_LCM/theory.cpp
@@ -0,0 +1,35 @@
+/* **********Highest Common Factor and Lowest Common Multiple (HCF & LCM)***********
+
+******* Calculate LCM ********
+1.
+ LCM: 2|4,8,6
+        -----
+       2|2,4,3
+         -----
+         1,2,3
+    Now LCM= 2×2×1×2×3 = 24
+2. So what is doing internally, let's assume LCM(9,12)
+Ans: So multiples of 9: 9×18×27×36×45×54
+     So multiples of 12: 12×24×36×48×60
+
+     now here Lowest Common multiple is 36, So LCM(9,12)=36.
+
+
+******* Calculate HCF ********
+1.
+HCF: 3|6,9,12
+       ------
+       2,3,4
+
+       now see 2 & 4 can be divided by 2, but we can do it for LCM, NOT HCF.
+       Now, HCF of 6,9,12 is 3.
+
+2. So what is going internally,
+Ans: Prime Factorization of 6: 1×2×3×6
+     Prime Factorization of 9: 1×3×9
+     Prime Factorization of 12: 1×2×3×4×6×12
+
+     Now check Highest Prime Factor common in all casees is 3.
+     So, HCF(6,9,12)=3
+
+*/
