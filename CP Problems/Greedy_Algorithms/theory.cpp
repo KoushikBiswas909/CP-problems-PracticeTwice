b926856deee0fe6659a1947a4f47d91cf6a23853
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-09-19 16:36:04 +0530

    Update theory.cpp

--- CP Problems/Greedy_Algorithms/theory.cpp
+++ CP Problems/Greedy_Algorithms/theory.cpp
@@ -42,5 +42,6 @@ Algo from- Git repo & Narasimha
 https://leetcode.com/discuss/interview-question/1065228/Job-sequencing-Problem
 https://www.geeksforgeeks.org/job-sequencing-problem/
 https://www.geeksforgeeks.org/job-sequencing-using-disjoint-set-union/
+https://www.youtube.com/watch?v=4JzWkuCAoGU&ab_channel=CodeNCode
 
 */
