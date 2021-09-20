ca340248ff9b842b45e9c174299554d818f05f2b
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-09-20 13:48:56 +0530

    Update theory.cpp

--- CP Problems/Greedy_Algorithms/theory.cpp
+++ CP Problems/Greedy_Algorithms/theory.cpp
@@ -37,11 +37,8 @@ Didnot code it caz i don't know some priority queue
 Theory from- Abdul Bari
 Algo from- Git repo & Narasimha
 
+************************  Job Scheduling Problem  **********************
 
 
-https://leetcode.com/discuss/interview-question/1065228/Job-sequencing-Problem
-https://www.geeksforgeeks.org/job-sequencing-problem/
-https://www.geeksforgeeks.org/job-sequencing-using-disjoint-set-union/
-https://www.youtube.com/watch?v=4JzWkuCAoGU&ab_channel=CodeNCode
 
 */
