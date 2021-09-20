3df60883f738bee31203f3c0531b370f3724e2aa
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-09-20 13:47:35 +0530

    Create Important_Link.cpp

---
+++ CP Problems/Greedy_Algorithms/Job_Scheduling_Problem/Important_Link.cpp
@@ -0,0 +1,4 @@
+https://leetcode.com/discuss/interview-question/1065228/Job-sequencing-Problem
+https://www.geeksforgeeks.org/job-sequencing-problem/
+https://www.geeksforgeeks.org/job-sequencing-using-disjoint-set-union/
+https://www.youtube.com/watch?v=4JzWkuCAoGU&ab_channel=CodeNCode
