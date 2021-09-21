1856408e6c6d3a082c1ac38b3936bea4ddfc09ba
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-09-21 22:01:21 +0530

    Create 1.Introduction.java

---
+++ Software Engineering/1.Introduction.java
@@ -0,0 +1,23 @@
+/*
+
+**** Software Engineering ****
+
+**** Syllabus ****
+1. Introduction
+2. Software Development Life Cycle
+3. Requirment Analysis
+4. Software Project Management
+5. Software Design
+6. Coding and testing
+7. Maintence
+8. Quality Management, Rouse 
+
+**** Definition and Evolution ****
+• It is systematic, disciplined, cost-effective techniques for
+software development.
+• Engineering approach to develop a software.
+
+
+
+
+*/
