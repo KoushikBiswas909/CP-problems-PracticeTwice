4a74359ffdb65b27b254e474e81834d0d9328d9b
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-09-21 23:09:55 +0530

    Create Theory.hs

---
+++ Software Engineering/Software Development Life Cycle/Theory.hs
@@ -0,0 +1,41 @@
+/*
+
+**** Software Development Life Cycle *****
+
+Planning->Defining/Analysis->Designing->Coding/Implementation->Testing->Deploy/Maintanace
+
+
+**** Classical Waterfal Model ****
+
+Feasibility Study
+      ⬇
+Requirments Analysis and Specification
+      ⬇
+    Design
+      ⬇
+Coding and Unity testing
+      ⬇
+System testing and Integration
+      ⬇
+    Maintence
+
+
+Advantages of waterfall Model:
+1.Basic Model
+2.Simple and easy
+3.Small project can only implemented by this.
+
+Disadvantages of waterfall Model:
+1. No feed back in all phase
+2.No experiment in all phase
+3.No parallelism, when one team works, others are free.
+4.High risk
+5.60% efforts in Maintanance phase.
+
+
+**** Iterative Waterfall Model ****
+
+The main deference is Feedback Method.
+Check Itertive_waterfall_method.png
+
+*/
