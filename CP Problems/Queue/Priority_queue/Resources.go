79df1044fb8f834c0249ed0cf3776f94a3412ccb
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-10-26 11:31:21 +0530

    Update Resources.go

--- CP Problems/Queue/Priority_queue/Resources.go
+++ CP Problems/Queue/Priority_queue/Resources.go
@@ -1,1 +1,7 @@
-1. https://www.softwaretestinghelp.com/priority-queue-in-cpp/amp/
+1. First learn from this video-
+   https://www.youtube.com/watch?v=oUf_cOKbzLY&ab_channel=Innoskrit
+
+2. Learn basic code from here
+   https://www.mygreatlearning.com/blog/priority-queue-in-cpp/
+
+3. https://www.softwaretestinghelp.com/priority-queue-in-cpp/amp/
