2c998db82ebbee8044afa24b22e9c3557bda2581
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2022-04-06 08:38:43 +0530

    Create Most_asked_qus.dart

---
+++ Python_Django_learning_notes/Basic_Python/Python Interview Questions/Most_asked_qus.dart
@@ -0,0 +1,65 @@
+/*
+All question list [Do like to keep it at Top]
+***Mostly asked questions***
+1.What is the difference between List and Tupples?
+2.What is a decorator? Explain it with an example.
+3.What is the difference between List comprehension, Dict comprehension and Generator
+4.How is memory management in python?
+5.What is the difference between Generator and Iterator?
+6.What is __init__ in python?
+7.What is difference between library and module in python?
+8.What is the difference between Range and Xrange in python?
+9.What is Generator explain with an example.[YIELD]
+10.What are the datatypes in python? [explain mutable and unmutable]
+
+
+***Commonly asked Questions***
+1.What is ternary operator in python?
+2.What is Inheritance and how it achived in Python?
+3.What is global and LOcal variable in python?
+4.Explain break,continue and pass keyword in python.
+5.What is self keyword in python?
+6.Difference between pickling and unpickling.
+7.Explain functions of List, Tuples, Set, Dictonary.
+8.What is python Iterator?
+9.Explain type conversion in python.
+10.What is diffrence between *args and **kwargs explain in detail.
+11.What is the purpose of "with" and "open" statement in python?
+12.Explain different ways to read and write a file in python.
+13.What is pythonpath?
+14.How exception is handeled in python? [try,except,else,finally]
+15.Difference between python 2.0 and python 3.0
+16.What is PIP in python?
+17.Where python is used? [Web,AI,ML]
+18.How to use "F" string and format or replacement operator in python?
+19.What is difference between Abstraction and Encapsulation?
+20.How to get the list of all the keys in a dictonary?
+
+***Tricky Questions***
+1.Does python support Multiple Inheritance explain it. [Diamond Problem]
+2.How to initialize empty List, empty Tuple, empty Dictonary without using their 
+actual name.? [kind of by using [{()}] this ]
+3.Difference between .py and .pyc in python
+4.How slicing works in string manupulation in python? [::-1](Practice string manupulation)
+5.How we concatenate two tuple?
+6.What is the difference between a python array and python list?
+7.what is difference between _a, __a, ___a___ in python?
+8.How to read multiple values from a single input? [use split()]
+9.How to copy and delete a dictionary?
+10.What is difference between Anonymous and lambda fuctions? [Read lambda func carefully]
+
+***Advanced Questions***
+1.How to achive Multi-threding and Multi-Processing in python?
+2.What is GIL (Global Interpreter Lock)
+3.How class and object are created in python?
+4.Explain recursion and use it to reverse a List.
+5.Explain Namespace and its types in python.
+6.What is the unittest in python?
+7.How to use MAP, FILTER, and REDUCE in python?
+8.How an object be copied in python?
+9.What is monkey paching in python?
+10.Difference between shallow copy and deep copy?
+11.What is Operator overloading and Dunder method?
+12.Explain or WAP to display a particular pattern.
+
+*/
